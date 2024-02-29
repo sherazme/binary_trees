6cdd8b14ca96c34e5cb7bc3786e257c55721a7a1
sherazme <sheraz.m.e@gmail.com>
2024-02-29 13:37:55 -0800

    binary

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - checks if node is leaf
+ * @node: pointer to node
+ * Return: 1 if node is leaf and 0 otherwise
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node != NULL && node->left == NULL && node->right == NULL)
+		return (1);
+	return (0);
+}
